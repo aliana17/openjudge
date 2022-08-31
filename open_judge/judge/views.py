0ed47cde5b9340c869fc1ffc65cbf2ea04307f6b
Anchal Agarwal <agarwalanchal72@gmail.com>
2022-09-01 00:12:54 +0530

    Fixes

--- open_judge/judge/views.py
+++ open_judge/judge/views.py
@@ -8,12 +8,11 @@ import subprocess
 
 path = Path(__file__).resolve().parent.parent
 SUBMISSION_DIR = path/'submissions'
-QUESTIONS_DIR = '../questions/1/statement'
 wrap_map = None
 
 #An endpoint to display the code editor to user
 def workspace(request):
-    return render(request, "judge/workspace.html", context={'d_path': QUESTIONS_DIR})
+    return render(request, "judge/workspace.html", context={})
 
 #It is used to generate the command need to run the code
 def new_attempt(answer, language):
